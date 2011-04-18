df5a42ed7a7e4591f972b39502deeb90a6069e69
James.liu.proj@gmail.com <James.liu.proj@gmail.com@fbda0d0e-6bca-d881-b0b5-846aa2e7aeb4>
2011-04-18 08:12:59 +0000

    init project

--- trunk/infiniteGallery/src/com/jms/InfiniteGalleryActivity.java
+++ trunk/infiniteGallery/src/com/jms/InfiniteGalleryActivity.java
@@ -1,6 +1,20 @@
+/*
+ * Copyright (C) 2007 The Infinite Gallery Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.jms;
-
-
 
 import android.app.Activity;
 import android.content.Context;
@@ -12,35 +26,24 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 
 public class InfiniteGalleryActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-        
-        InfiniteGallery gallery = (InfiniteGallery)findViewById(R.id.mygallery);
-        gallery.setAdapter(new ImageAdapter(this));
-    }
-    
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		InfiniteGallery gallery = (InfiniteGallery) findViewById(R.id.mygallery);
+		gallery.setAdapter(new ImageAdapter(this));
+	}
+
 	public class ImageAdapter extends BaseAdapter {
 		private LayoutInflater inflater = null;
 
-        private final Integer[] imageDataList = {
-                R.drawable.a,
-                R.drawable.b,
-                R.drawable.c,
-                R.drawable.d,
-                R.drawable.e,
-                R.drawable.f,
-                R.drawable.g,
-                R.drawable.h,
-                R.drawable.i,
-                R.drawable.j,
-                R.drawable.k,
-                R.drawable.l
-        };
-		
+		private final Integer[] imageDataList = { R.drawable.a, R.drawable.b,
+				R.drawable.c, R.drawable.d, R.drawable.e, R.drawable.f,
+				R.drawable.g, R.drawable.h, R.drawable.i, R.drawable.j,
+				R.drawable.k, R.drawable.l };
+
 		public ImageAdapter(Context c) {
 			inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		}
@@ -63,11 +66,11 @@ public class InfiniteGalleryActivity extends Activity {
 
 		public View getView(int position, View convertView, ViewGroup parent) {
 			View view = convertView;
-			
+
 			if (view == null) {
 				view = inflater.inflate(R.layout.itemrender, parent, false);
 			}
-			
+
 			((ImageView) view).setImageResource(imageDataList[position]);
 			return view;
 		}
